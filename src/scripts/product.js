1c0719d86c020e3c57bf2cb276e472781f798d26
Kirill Kapralov <Kapralov1501@gmail.com>
2024-06-02 16:27:45 +0300

    dist v1.0.0

--- src/scripts/product.js
+++
@@ -1,8 +0,0 @@
-const productBtn = $('.choose__cards .btn');
-productBtn.click(function () {
-    const product = $(this).parent().parent().prev().prev().text();
-    const size = $('input[name="size"]:checked').val();
-    $('.input_product').removeClass('notValidate');
-    $('#product').val(product + ' ' + size);
-    $('.order')[0].scrollIntoView({ behavior: "smooth"});
-});
